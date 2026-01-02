6a80ad1bbb9475f16e171ffea2bfd5f18bb29d10
unonibes4a <51521414+unonibes4a@users.noreply.github.com>
2026-01-02 16:26:45 -0500

    Update libsimple.js

--- libsimple.js
+++ libsimple.js
@@ -684,7 +684,7 @@ class GioAudioTeclaDic302025 {
             width: 100%; 
             height: 100%;  
             margin: 0;  
-            padding: 0;  
+            padding: 1rem  1rem 1rem 1rem;  
         }
 
 			   .todobox {
